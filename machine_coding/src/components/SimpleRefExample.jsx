005d423cbe5d75d4034f9e51ef3d617e71a4fbdc
braju007 <erbrj7@gmail.com>
2025-07-23 23:48:20 +0530

    simple ref example

---
+++ machine_coding/src/components/SimpleRefExample.jsx
@@ -0,0 +1,9 @@
+import React,{useState, useEffect, useRef} from 'react';
+
+export const SimpleRefExample=()=>{
+    return(
+        <div>
+            <h1>Simple Ref Example:</h1>
+        </div>
+    )
+}
