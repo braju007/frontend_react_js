ba5ab91fa93f2e8fd6f9d7877b96f4d923f1ede8
braju007 <erbrj7@gmail.com>
2025-07-24 22:54:12 +0530

    Added prettier to the project and changes accordingly

--- machine_coding/src/components/SimpleCounter.jsx
+++ machine_coding/src/components/SimpleCounter.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 
-export const SimpleCounter=()=>{
-    const [count, setCount] = useState(0);
+export const SimpleCounter = () => {
+  const [count, setCount] = useState(0);
 
-    const incrementCount = () =>{
-        setCount(prev => prev + 1);
-    }
+  const incrementCount = () => {
+    setCount((prev) => prev + 1);
+  };
 
-    const decrementCount = () =>{
-        setCount(prev => prev - 1);
-    }
+  const decrementCount = () => {
+    setCount((prev) => prev - 1);
+  };
 
-    return(
-        <div>
-            <h1>Counter Example:</h1>
-            <h3>Count is: {count}</h3>
-            <button onClick={incrementCount}>Increment</button>
-            <button onClick={decrementCount}>Decrement</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Counter Example:</h1>
+      <h3>Count is: {count}</h3>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+    </div>
+  );
+};
