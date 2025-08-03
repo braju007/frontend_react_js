8df58740b31469a737f5d7df75048f116b594384
braju007 <erbrj7@gmail.com>
2025-08-03 13:14:50 +0530

    Add debounce custom hook

---
+++ machine_coding/src/components/customHooks/useDebounce.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+export const useDebounce = (value, delay) => {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    let handler = setTimeout(() => {
+      setDebounced(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+};
